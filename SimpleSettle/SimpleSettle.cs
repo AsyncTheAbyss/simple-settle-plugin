99f7e7f47962542eb4f66ad66f103ed6322e39c8
Donut313 <108238272+donut313@users.noreply.github.com>
2023-11-12 15:01:32 -0500

    added plugin config

--- SimpleSettle/SimpleSettle.cs
+++ SimpleSettle/SimpleSettle.cs
@@ -2,10 +2,35 @@ using TerrariaApi.Server;
 using Terraria;
 using System.Timers;
 using TShockAPI;
+using Newtonsoft.Json;
 using System.Threading.Tasks;
+using TShockAPI.Configuration;
+using TShockAPI.Hooks;
 
 namespace SimpleSettle;
 
+// set up config manager
+public class ConfigManager {
+    public bool Message = false;
+    public bool MessageOnlyInConsole = true;
+    public bool MaxSettle = true;
+    public int MaxSetttleAmount = 10000;
+    public int SettleTime = 300;
+    public string settlemessage = "Settling liquids.";
+    public string settlehighmessage = "Settling liquids due to high amount of liquids.";
+    public void write()
+    {
+        File.WriteAllText(SimpleSettle.path,JsonConvert.SerializeObject(this,Formatting.Indented));
+    }
+    public ConfigManager read() {
+        // check if our config does not exist
+        if (!File.Exists(SimpleSettle.path))
+        {
+            return new ConfigManager();
+        }
+        return JsonConvert.DeserializeObject<ConfigManager>(File.ReadAllText(SimpleSettle.path));
+    }
+}
 /// <summary>
 /// The main plugin class should always be decorated with an ApiVersion attribute. The current API Version is 2.1
 /// </summary>
@@ -31,7 +56,9 @@ public class SimpleSettle : TerrariaPlugin
     /// A short, one-line, description of the plugin's purpose.
     /// </summary>
     public override string Description => "a simple plugin that settles all the liquid on the server every 5 minutes";
-
+    // config setup 
+    public static string path = Path.Combine(TShock.SavePath, "SimpleSettle.json");
+    public static ConfigManager config = new ConfigManager();
     // timer variables
     private System.Threading.Timer liquidTimer;
     private System.Threading.Timer highLiquidTimer;
@@ -50,9 +77,28 @@ public class SimpleSettle : TerrariaPlugin
     /// </summary>
     public override void Initialize()
     {
-        ServerApi.Hooks.GamePostInitialize.Register(this, LoadUpTimer);
+        // set up config
+        GeneralHooks.ReloadEvent += OnReload;
+        if (File.Exists(path))
+            config = config.read();
+        else 
+            config.write();
+        ServerApi.Hooks.GamePostInitialize.Register(this, LoadUpTimers);
     }
-
+    private void OnReload(ReloadEventArgs args)        
+    {
+        if (File.Exists(path))
+             config = config.read();
+        else 
+            config.write();
+        // restart the timers to apply altered time
+        DestroyTimers();
+        SetLiquidTimer();
+        if (config.MaxSettle)
+        {
+        SetHighLiquidTimer();
+        }
+    }
     /// <summary>
     /// Performs plugin cleanup logic
     /// Remove your hooks and perform general cleanup here
@@ -62,24 +108,18 @@ public class SimpleSettle : TerrariaPlugin
         if (disposing)
         {
             // unhook
-            ServerApi.Hooks.GamePostInitialize.Deregister(this, LoadUpTimer);
-            // dispose child objects
-            liquidTimer.Dispose();
-            highLiquidTimer.Dispose();
-            // set large objects to null
-            liquidTimer = null;
-            highLiquidTimer = null;
+            ServerApi.Hooks.GamePostInitialize.Deregister(this, LoadUpTimers);
+            // destroy timers
+            DestroyTimers();
         }
         base.Dispose(disposing);
     }
 
     // create timer for the liquid
-    private static bool hassettledwithnooneon = false;
-    public static bool didliquids = false;
-
+    private static bool HasSettledWithNoneOn = false;
     private void SetLiquidTimer()
     {
-        liquidTimer = new System.Threading.Timer(SettleLiquids, null, 300000, 300000);
+        liquidTimer = new System.Threading.Timer(SettleLiquids, null, 1000 * config.SettleTime, 1000 * config.SettleTime);
     }
 
     private void SetHighLiquidTimer()
@@ -95,52 +135,66 @@ public class SimpleSettle : TerrariaPlugin
         // check if a player is active
         if (TShock.Players[0].Active)
         {
+            // set the bool to false because someone is active on the server
+            HasSettledWithNoneOn = false;
+        }
+        else {
+            // set the bool to true now that liquids have been settled with no one on the server
+            HasSettledWithNoneOn = true;
+        }
             // check if there is literally anything to settle
-            if (liquidCount > 100)
+            if (liquidCount > 100 && !HasSettledWithNoneOn)
             {
                 // settle liquid
                 Liquid.StartPanic();
                 // show message
-                TShockAPI.TSPlayer.All.SendInfoMessage("Settling liquids.");
-                // set the bool to false because someone is active on the server
-                hassettledwithnooneon = false;
-            }
-        }
-        else
-        {
-            // check if it has settled with no one on the server
-            if (!hassettledwithnooneon)
-            {
-                // check if there is literally anything to settle
-                if (liquidCount > 100)
+                if (config.Message && !config.MessageOnlyInConsole)
                 {
-                    // settle liquid
-                    Liquid.StartPanic();
-                    // show message
-                    TShockAPI.TSPlayer.All.SendInfoMessage("Settling liquids.");
-                    // set the bool to true now that liquids have been settled with no one on the server
-                    hassettledwithnooneon = true;
+                TShockAPI.TSPlayer.All.SendInfoMessage(config.settlemessage);
                 }
+                if (config.MessageOnlyInConsole)
+                {
+                    Console.WriteLine(config.settlemessage);
+                }
+                
             }
-        }
+        
+                    
     }
 
     private void SettleLiquidsHigh(object state)
     {
         // calculate liquid count
         int liquidCount = Liquid.numLiquid + LiquidBuffer.numLiquidBuffer;
-            if (liquidCount > 10000)
+            if (liquidCount > config.MaxSetttleAmount)
             {
                 // settle liquid
                 Liquid.StartPanic();
                 // show message
-                TShockAPI.TSPlayer.All.SendInfoMessage("Settling liquids due to high amount of liquids.");
+                if (config.Message && !config.MessageOnlyInConsole)
+                {
+                TShockAPI.TSPlayer.All.SendInfoMessage(config.settlehighmessage);
+                }
+                if (config.MessageOnlyInConsole)
+                {
+                Console.WriteLine(config.settlehighmessage);
+                }
             }
     }
 
-    private void LoadUpTimer(EventArgs args)
+    private void LoadUpTimers(EventArgs args)
     {
         SetLiquidTimer();
+        if (config.MaxSettle)
+        {
         SetHighLiquidTimer();
+        }
+    }
+    private void DestroyTimers()
+    {
+        liquidTimer.Dispose();
+        highLiquidTimer.Dispose();
+        liquidTimer = null;
+        highLiquidTimer = null;
     }
 }
